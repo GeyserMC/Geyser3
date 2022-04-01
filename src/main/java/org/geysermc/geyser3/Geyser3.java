c9917fa97b48f61f389b12e103761f56a409f3ca
RednedEpic <redned235@gmail.com>
2022-04-01 07:00:01 -0500

    Initial commit

---
+++ src/main/java/org/geysermc/geyser3/Geyser3.java
@@ -0,0 +1,39 @@
+package org.geysermc.geyser3;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Geyser3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getCommand("geyser3").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase("geyser3")) {
+            return true;
+        }
+
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be run by a player.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                for (int y = -1; y <= 5; y++) {
+                    player.getLocation().getBlock().getRelative(x, y, z).setType(Material.AIR);
+                }
+            }
+        }
+
+        player.getLocation().getBlock().getRelative(0, 4, 0).setType(Material.WATER);
+        return true;
+    }
+}
